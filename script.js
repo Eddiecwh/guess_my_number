0b9b8053dfcf530e5db5c6f8475594c04b40b5dc
unknown <eddiecwh@gmail.com>
2023-08-21 15:17:39 -0500

    Adding game functionality and hiscore logic

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 "use strict";
 
-const secretNum = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNum;
+let secretNum = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -14,6 +15,14 @@ document.querySelector(".check").addEventListener("click", function () {
     if (guess === secretNum) {
       if (score > 0) {
         document.querySelector(".message").textContent = "You got it!";
+        document.querySelector("body").style.backgroundColor = "#60b347";
+        document.querySelector(".number").style.width = "30rem";
+        document.querySelector(".number").textContent = secretNum;
+        // Set hiscore
+        if (score > highScore) {
+          highScore = score;
+          document.querySelector(".highscore").textContent = highScore;
+        }
       } else {
         document.querySelector(".message").textContent =
           "You got it, but you have 0 pts, GAME OVER!";
@@ -36,4 +45,21 @@ document.querySelector(".check").addEventListener("click", function () {
       }
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  // set score back to 20
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  // turn screen back to black
+  document.querySelector("body").style.backgroundColor = "#222";
+  // turn number back to ?
+  document.querySelector(".number").textContent = "?";
+  // turn width of box back to 15 rem
+  document.querySelector(".number").style.width = "15rem";
+  // refresh secretNum
+  document.querySelector(".guess").value = "";
+  document.querySelector(".message").textContent = "Start guessing...";
+
+  secretNum = Math.trunc(Math.random() * 20) + 1;
 });
