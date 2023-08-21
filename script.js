94d58220c904f90758a1c30e3498e579721a2f46
unknown <eddiecwh@gmail.com>
2023-08-21 15:32:36 -0500

    Refactoring code

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 "use strict";
 
 let secretNum = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
-
 let highScore = 0;
+
+function displayMessage(message) {
+  document.querySelector(".message").textContent = message;
+}
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -14,7 +16,7 @@ document.querySelector(".check").addEventListener("click", function () {
   } else {
     if (guess === secretNum) {
       if (score > 0) {
-        document.querySelector(".message").textContent = "You got it!";
+        displayMessage("You got it!");
         document.querySelector("body").style.backgroundColor = "#60b347";
         document.querySelector(".number").style.width = "30rem";
         document.querySelector(".number").textContent = secretNum;
@@ -24,24 +26,16 @@ document.querySelector(".check").addEventListener("click", function () {
           document.querySelector(".highscore").textContent = highScore;
         }
       } else {
-        document.querySelector(".message").textContent =
-          "You got it, but you have 0 pts, GAME OVER!";
+        displayMessage("You got it, but you have 0 pts, GAME OVER!");
       }
-    } else if (guess > secretNum) {
-      if (score > 0) {
-        document.querySelector(".message").textContent = "Too High!";
+    } else if (guess !== secretNum) {
+      if (score > 1) {
+        displayMessage(guess > secretNum ? "Too High!" : "Too Low!");
         score--;
         document.querySelector(".score").textContent = score;
       } else {
-        document.querySelector(".message").textContent = "0 pts, GAME OVER!";
-      }
-    } else {
-      if (score > 0) {
-        document.querySelector(".message").textContent = "Too Low!";
-        score--;
-        document.querySelector(".score").textContent = score;
-      } else {
-        document.querySelector(".message").textContent = "0 pts, GAME OVER!";
+        displayMessage("0 pts, GAME OVER!");
+        document.querySelector(".score").textContent = 0;
       }
     }
   }
@@ -59,7 +53,7 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector(".number").style.width = "15rem";
   // refresh secretNum
   document.querySelector(".guess").value = "";
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
 
   secretNum = Math.trunc(Math.random() * 20) + 1;
 });
